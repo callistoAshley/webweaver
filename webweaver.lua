c6203a42e59afc0a63705faeb5a43b3796bd4f0c
callistoAshley <callistoashley@proton.me>
2024-06-29 18:56:16 +1000

    use a shitty hack to allow running outside of webweaver.lua's directory

--- webweaver.lua
+++ webweaver.lua
@@ -1,5 +1,7 @@
 #!/usr/bin/env lua
 -- TODO: records, blog management including config and index generation
 
-package.path = package.path .. ";" .. "./src/?.lua"
+-- bad awful no good hack: this won't work if webweaver is added to the PATH
+-- FIXME: don't do this
+package.path = string.format("%s;%s/src/?.lua", package.path, arg[0]:gsub("webweaver.lua$", ""):gsub("webweaver$", ""))
 require("main")

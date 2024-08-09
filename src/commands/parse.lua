4fa19a225b29f955d36af02305114af53dbfd87f
callistoAshley <callistoashley@proton.me>
2024-08-09 22:08:25 +1000

    implement parse comment

--- src/commands/parse.lua
+++ src/commands/parse.lua
@@ -1,7 +1,11 @@
 local CmdHelpers = require("command-helpers")
+local Helpers = require("helpers")
+local Parser = require("parser")
 
 local function parse_func(cmd)
-    print("i am the parse func")
+    local input = cmd:get_arg_by_name("-i"):get_value_by_name("input").value
+    local parsed = Parser:parse(Helpers.file_text(input) or error(string.format("No such file `%s`", input)))
+    print(parsed)
 end
 
 return CmdHelpers.new_command
@@ -11,4 +15,17 @@ return CmdHelpers.new_command
     long_help = 
     [[Invoke the parser without generating a new blog post.]],
     func = parse_func,
+
+    possible_args =
+    {
+        CmdHelpers.new_arg
+        (
+            {"-i", "--input"},
+            "Specify the input source file.",
+            CmdHelpers.ArgType.REQUIRED,
+            {
+                CmdHelpers.new_value("input", CmdHelpers.ValueType.REQUIRED)
+            }
+        ),
+    }
 }
